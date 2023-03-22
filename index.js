1403bda538c1815a8a0a50f47f04fdf04fa62df2
bigman <terrence.chebii@student.moringaschool.com>
2023-03-22 11:27:09 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+
+
 function addingEventListener() {
-}
+    const input = document.getElementById("input")
+    input.addEventListener('click' , addingEventListener)
+    alert('I was clicked')
+} 
+
